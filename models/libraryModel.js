84f0f5313d8b59a1c850c2f6a399483c9a96ffb8
afolabi <yusufafolabi95@gmail.com>
2024-07-03 18:00:58 +0100

    update

--- models/libraryModel.js
+++ models/libraryModel.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const librarySchema = new mongoose.Schema({
     totalUser: { type: Number, default: 0 },
     totalDocument: { type: Number, default: 0 },
-    totalCategroy: { type: Number, default: 0 },
+    totalCategory: { type: Number, default: 0 },
     totalReading: { type: Number, default: 0 },
     totalDownload: { type: Number, default: 0 }
 });
